ec469e1bb9ee93db18da95419d4cd578e915a0e2
mcbrown84uk <mcbrown84uk@hotmail.com>
2025-03-13 20:24:36 +0000

    jumbotron

--- scripts/program.js
+++ scripts/program.js
@@ -1,17 +1,27 @@
-function LoadPage(){
+function LoadPage() {
 
-    //test
     var body = document.body;
 
     body.appendChild(CreateDOM());
-
-
 }
 
-function CreateDOM(){
+function CreateDOM() {
     var container = document.createElement("div");
+    container.classList.add('container-fluid');
 
-    container.innerText = "Mike Does Stuff";
+    var jumbotron = document.createElement('div');
+    jumbotron.classList.add('mt-4', 'p-5', 'bg-primary', 'text-white', 'rounded');
+
+    var jumboTronHeader = document.createElement('div');
+    jumboTronHeader.innerText = "Mike Does Stuff";
+
+    var jumbotronSubHeader = document.createElement('p');
+    p.innerText = "My Corner of the internet";
+
+    jumbotron.appendChild(jumboTronHeader);
+    jumbotron.appendChild(jumbotronSubHeader);
+
+    container.appendChild(jumbotron);
 
     return container;
 }
