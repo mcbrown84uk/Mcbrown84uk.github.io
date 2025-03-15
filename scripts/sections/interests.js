84d3f2cc56eede2ff92b94ca54bd8aeaa547d954
mcbrown84uk <mcbrown84uk@hotmail.com>
2025-03-15 22:34:14 +0000

    reformat

--- scripts/sections/interests.js
+++ scripts/sections/interests.js
@@ -1,42 +1,25 @@
-function CreateInterests(){
+function CreateInterests() {
     var section = document.getElementById("interests");
 
     var container = document.createElement('div');
     var row = document.createElement('div');
-    var legoContainer = document.createElement('div');
-    legoContainer.id = 'legoContainer';
-    var vinylContainer = document.createElement('div');
-    vinylContainer.id = 'vinylContainer'
-    var travelContainer = document.createElement('div');
-    travelContainer.id = 'travelContainer';
 
     container.classList.add('container-fluid', 'h-100', 'g-0')
-    
     row.classList.add('row', 'h-100', 'g-0');
 
-    legoContainer.classList.add('col-md-4', 'h-md-100', 'interests-container', 'position-relative');
-    vinylContainer.classList.add('col-md-4', 'h-md-100', 'interests-container', 'position-relative');
-    travelContainer.classList.add('col-md-4', 'h-md-100', 'interests-container', 'position-relative');
+    for (var interest of ['Lego', 'Vinyl', 'Travel']) {
+        var interestContainer = document.createElement('div');
+        interestContainer.id = interest.toLowerCase() + 'Container';
+        interestContainer.classList.add('col-md-4', 'h-md-100', 'interests-container', 'position-relative');
+        
+        var title = document.createElement('a');
+        title.classList.add('interests-title', 'position-absolute', 'top-50', 'start-50', 'translate-middle', 'p-2', 'w-100', 'text-center');
+        title.innerText = interest;
+        title.href = '#' + interest.toLowerCase();
+        interestContainer.appendChild(title);
 
-    var legoTitle = document.createElement('a');
-    legoTitle.classList.add('interests-title', 'position-absolute', 'top-50', 'start-50', 'translate-middle', 'p-2', 'w-100', 'text-center');
-    legoTitle.innerText = 'Lego';
-    legoTitle.href = "#lego";
-    legoContainer.appendChild(legoTitle);
-
-    var vinylTitle = document.createElement('a');
-    vinylTitle.classList.add('interests-title', 'position-absolute', 'top-50', 'start-50', 'translate-middle', 'p-2', 'w-100', 'text-center');
-    vinylTitle.innerText = 'Vinyl';
-    vinylContainer.appendChild(vinylTitle);
-
-    var travelTitle = document.createElement('a');
-    travelTitle.classList.add('interests-title', 'position-absolute', 'top-50', 'start-50', 'translate-middle', 'p-2', 'w-100', 'text-center');
-    travelTitle.innerText = 'Travel';
-    travelContainer.appendChild(travelTitle);
-
-    row.appendChild(legoContainer);
-    row.appendChild(vinylContainer);
-    row.appendChild(travelContainer);
+        row.appendChild(interestContainer);
+    }
 
     container.appendChild(row);
 
