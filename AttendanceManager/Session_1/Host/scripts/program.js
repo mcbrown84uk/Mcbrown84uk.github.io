ed05f4d267ed719866bfd8b4f119eabba1a2cf60
mcbrown84uk <mcbrown84uk@hotmail.com>
2025-03-20 21:55:53 +0000

    qr

--- AttendanceManager/Session_1/Host/scripts/program.js
+++ AttendanceManager/Session_1/Host/scripts/program.js
@@ -1,12 +1,30 @@
-function LoadAttendees(){
-    fetch('../')
-    .then(response => response.json()) // Parse JSON
-    .then(data => LoadVinyl(data)) // Work with JSON data
-    .catch(error => console.error('Error fetching JSON:', error));
+function LoadAttendees() {
+
 
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('aid');
 
-    var attendeesList = document.getElementById('attendeesList');
-    attendeesList.innerText = myParam;
+    //var attendeesList = document.getElementById('attendeesList');
+    //attendeesList.innerText = myParam;
+
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+        "qrreader",
+        { fps: 10, qrbox: { width: 250, height: 250 } },
+    /* verbose= */ false);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+}
+
+
+
+
+
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
 }
