651fee3e199831930e6ad64a4c7a9379ea120184
mcbrown84uk <mcbrown84uk@hotmail.com>
2025-03-20 21:14:26 +0000

    test

--- AttendanceManager/Session_1/Attendee/scripts/program.js
+++ AttendanceManager/Session_1/Attendee/scripts/program.js
@@ -2,5 +2,5 @@ function LoadAttendeePage() {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('aid')
 
-    new QRCode(document.getElementById("divQrCode"), "http://meetingAttender.com/meetingid/" + urlParams.get('aid'));
+    new QRCode(document.getElementById("divQrCode"), "http://127.0.0.1:5500/AttendanceManager/Session_1/Host/index.html?aid=" + urlParams.get('aid'));
 }
--- AttendanceManager/Session_1/Host/scripts/program.js
+++ AttendanceManager/Session_1/Host/scripts/program.js
@@ -3,4 +3,10 @@ function LoadAttendees(){
     .then(response => response.json()) // Parse JSON
     .then(data => LoadVinyl(data)) // Work with JSON data
     .catch(error => console.error('Error fetching JSON:', error));
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get('aid');
+
+    var attendeesList = document.getElementById('attendeesList');
+    attendeesList.innerText = myParam;
 }
